5c97555e4494fbfd5bed8ed504818081c370effe
Chavez Harris <codedbychavez@gmail.com>
2023-10-24 22:49:20 -0300

    Added basic highlighting

--- src/scripts/home.ts
+++ src/scripts/home.ts
@@ -4,6 +4,16 @@ export const home = {
   // State is a global object that can be used to store data
   state: {
     counter: 0,
+    slots: [
+      {
+        name: "Feature Flag Name",
+        color: "green",
+      },
+      {
+        name: "Action",
+        color: "purple",
+      }
+    ]
   },
 
   init() {
@@ -13,7 +23,7 @@ export const home = {
     document.addEventListener("stateChange", home.syncState);
 
     // Setup the increment and decrement buttons
-    home.setupIncrementButton();
+    home.setupTextContainerEvents();
     home.setupDecrementButton();
   },
 
@@ -22,15 +32,79 @@ export const home = {
   },
 
   methods: {
-    highlightText: () => {
-      console.log("highlightText");
+
+    mouseSelectionTextHighLight: (parentElement: HTMLElement) => {
+      const selection = window.getSelection();
+
+      if (selection && selection.toString()) {
+        const text = selection.toString();
+
+        // Get all the spans in the parent element
+        // const spans = parentElement.getElementsByTagName("span");
+
+        const sentence = parentElement.innerText;
+        const { startIndex, endIndex } = home.methods.findStartAndEndIndex(
+          sentence,
+          text
+        );
+
+        // Create a range object for the selected text
+        const range = selection.getRangeAt(0);
+
+        // Create a new span element
+        const span = document.createElement("span");
+
+        // Pick a random color from the colors array
+        const randomColor = home.state.slots[0].color;
+
+        // Set the background color of the span element to the random color
+        span.classList.add(randomColor);
+
+        // Surround the selected text with the span element
+        range.surroundContents(span);
+
+        console.log(startIndex, endIndex);
+      }
+
+      // Pick a random color from the colors array
+      
+      // Set the background color of the child element to the random color
+      // childElement.classList.add(randomColor);
+
+      // Get the text content of the parent element
+      // const sentence = parentElement.innerText;
+
+      // Get the text content of the child element
+      // const text = childElement.innerText;
+
+      // Find the start and end index of the child text in the sentence
+      // const { startIndex, endIndex } = home.methods.findStartAndEndIndex(
+      //   sentence,
+      //   text
+      // );
+      // console.log(startIndex, endIndex);
+    },
+
+    findStartAndEndIndex: (sentence: string, text: string) => {
+      // Trim the text
+      const childTextContent = text.trim();
+      // Find the start index of the text
+      const startIndex = sentence.indexOf(childTextContent!);
+      // Find the end index of the text
+      const endIndex = startIndex! + childTextContent!.length;
+      return { startIndex, endIndex };
+    },
   },
 
-},
+  setupTextContainerEvents: () => {
+    const textContainer = document.getElementById("text-container");
+    // Add a double click event listener to all child elements of the text container
 
-  setupIncrementButton: () => {
-    const button = document.getElementById("text-container");
-    // button?.addEventListener("click", home.methods.incrementCounter);
+    textContainer?.addEventListener("mouseup", () =>
+      home.methods.mouseSelectionTextHighLight(textContainer!)
+    );
+
+    // textContainer?.addEventListener("dblclick", () => home.methods.highlightText(textContainer));
   },
 
   setupDecrementButton: () => {
